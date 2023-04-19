236b4d61c1a296099bb83155ecdd753edaf02b04
Bijan k <80640045+Bijan-K@users.noreply.github.com>
2023-04-19 09:10:02 +0430

    layout and format

--- components/MainHolder.js
+++ components/MainHolder.js
@@ -1,7 +1,22 @@
 export default function MainHolder() {
+  const test = (event) => {
+    event.preventDefault();
+    console.log('sdf');
+  };
+
+  const test2 = (event) => {
+    event.stopPropagation();
+    console.log('ds');
+  };
   return (
-    <div className="w-full h-full bg-slate-600 flex items-center justify-center">
-      <div className=" shadow shadow-orange-100 rounded-xl bg-slate-700 px-24 py-[20px]">
+    <div
+      className="w-full h-full bg-slate-600 flex items-center justify-center cursor-pointer"
+      onClick={test}
+    >
+      <div
+        className=" shadow shadow-orange-100 rounded-xl bg-slate-700 px-24 py-[20px]"
+        onClick={test2}
+      >
         <span className="text-white text-8xl">infrignable</span>
       </div>
     </div>
--- components/mainButtons/LogButton.js
+++ components/mainButtons/LogButton.js
@@ -1,6 +1,6 @@
 export default function LogButton() {
   return (
-    <button className="text-center bg-black text-white px-2 py-1 rounded-lg font-semibold shadow-slate-800 shadow">
+    <button className="text-center bg-black text-white px-2 py-1 rounded-lg font-semibold shadow-slate-800 shadow active:bg-slate-800">
       Log
     </button>
   );
